8eb6304299bc8939d61cc964cf233b4ad3fda708
Connor Herbert <minecraftiacy12034@gmail.com>
2015-02-11 22:38:41 -0500

    Awesome things are being packaged

--- src/co/q64/test/Main.java
+++ src/co/q64/test/Main.java
@@ -1,9 +1,16 @@
 package co.q64.test;
 
+import liketodance.Like;
+
 public class Main{
 	public static void main(String[] args) {
 		Welcomer welcomer = new Welcomer();
 		welcomer.sayHello();
+		Main mainer = new Main();
+		mainer.like();
 	}
-
+	public void like(){
+		Like like = new Like();
+		like.doSomething();
+	}
 }
---
+++ src/liketodance/Like.java
@@ -0,0 +1,10 @@
+package liketodance;
+
+public class Like {
+	public void doSomething() {
+		double doubleValue = 3.99;
+		int intResult = (int) Math.round(doubleValue);
+		System.out.println(intResult);
+	}
+
+}
